79deec34e895fe4fe0feac13bdc1ff0dd66c1ef9
Arjun Puri <apuri@plaid.com>
2022-04-09 18:39:35 +0530

    More updates

--- macos/sync.sh
+++ macos/sync.sh
@@ -2,7 +2,8 @@
 
 RSYNC='rsync -ar --delete --delete-excluded --progress'
 $RSYNC --exclude nvim/vim_runtime/.git --exclude nvim/vim_runtime/temp_dirs ~/.config/nvim .
-$RSYNC ~/.config/alacritty .
+$RSYNC ~/.config/alacritty/alacritty.yml .
 $RSYNC ~/.zshrc .
 $RSYNC ~/.tmux.conf .
 $RSYNC ~/.config/starship.toml .
+
